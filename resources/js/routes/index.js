ac6363ee4e2921bca0d814cc824911429e69a480
OthmanCharai <otmanchara3i0688@gmail.com>
2023-02-19 18:47:01 +0100

    confilt

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -4,7 +4,7 @@ import NotFound from "./../Pages/not-founded.vue";
 import ExamIndex from "./../Pages/exams/index.vue";
 import ExamStart from './../Pages/exams/start-exam.vue';
 import Question from "./../Pages/exams/question.vue"
-import examResult from "./../Pages/exams/examResult.vue";
+import examResult from "./../Pages/exams/examResult/index.vue";
 import Contact from "./../Pages/Contact/index.vue"
 
 const router = createRouter({
