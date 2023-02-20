ef94b635c4dc24fea162db9c6a37ac0bdf74a28b
hmellahi <hamzasenpai307@gmail.com>
2023-02-20 23:17:56 +0100

    add timer

---
+++ resources/js/stores/timer.js
@@ -0,0 +1,32 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const useTimerStore = defineStore("timer", () => {
+    const timeLeft = ref(0);
+    let timeInterval = null;
+
+    function resetTimer() {
+        timeLeft.value = 8;
+        if (timeInterval) {
+            clearInterval(timeInterval);
+        }
+    }
+
+    function startTimer(callback) {
+        timeInterval = setInterval(() => {
+            timeLeft.value--;
+            if (timeLeft.value === 0) {
+                clearInterval(timeInterval); // Stop the interval
+                callback();
+            }
+        }, 1000); // 1s
+    }
+
+    return {
+        timeLeft,
+        resetTimer,
+        startTimer,
+    };
+});
+
+//
