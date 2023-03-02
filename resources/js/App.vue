f009f4ada3552faee835e847271fca7ca19828a0
hmellahi <hamzasenpai307@gmail.com>
2023-03-02 17:11:57 +0100

    add the template

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,26 @@
-<template>
-    <div id="app">
-      
-      <router-view />
-    </div>
-  </template>
 <script setup>
+import { useTheme } from 'vuetify'
+import { useThemeConfig } from '@core/composable/useThemeConfig'
+import { hexToRgb } from '@layouts/utils'
 
+const {
+  syncInitialLoaderTheme,
+  syncVuetifyThemeWithTheme: syncConfigThemeWithVuetifyTheme,
+  isAppRtl,
+} = useThemeConfig()
+
+const { global } = useTheme()
+
+// ℹ️ Sync current theme with initial loader theme
+syncInitialLoaderTheme()
+syncConfigThemeWithVuetifyTheme()
 </script>
-<style lang="">
-    
-</style>
+
+<template>
+  <VLocaleProvider :rtl="isAppRtl">
+    <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
+    <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
+      <RouterView />
+    </VApp>
+  </VLocaleProvider>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,34 @@
 import "./bootstrap";
 
-import { createApp } from "vue";
-import { i18nVue } from "laravel-vue-i18n";
-import router from "./routes/index";
+/* eslint-disable import/order */
+import "@/@fake-db/db";
+import "@/@iconify/icons-bundle";
+import App from "@/App.vue";
+import ability from "@/plugins/casl/ability";
+import i18n from "@/plugins/i18n";
+import layoutsPlugin from "@/plugins/layouts";
+import vuetify from "@/plugins/vuetify";
+import { loadFonts } from "@/plugins/webfontloader";
+import router from "@/router";
+import { abilitiesPlugin } from "@casl/vue";
+import "@/@core/scss/template/index.scss";
+import "@/styles/styles.scss";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
+import I18nVue from "./@core/components/I18n.vue";
 
-import App from "./App.vue";
-
+loadFonts();
 const pinia = createPinia();
 
 const setupApp = async () => {
     if (window.location.href === "examResult") {
-        await import("@/helpers/style");
+        // await import("@/helpers/style");
     }
 
+
+    // Use plugins
     createApp(App)
-        .use(i18nVue, {
+        .use(I18nVue, {
             resolve: async (lang) => {
                 const langs = import.meta.glob("../../lang/*.json");
                 return await langs[`../../lang/${lang}.json`]();
@@ -23,6 +36,14 @@ const setupApp = async () => {
         })
         .use(router)
         .use(pinia)
+        .use(vuetify)
+        .use(createPinia())
+        .use(router)
+        .use(layoutsPlugin)
+        .use(i18n)
+        .use(abilitiesPlugin, ability, {
+            useGlobalProperties: true,
+        })
         .mount("#app");
 };
 
